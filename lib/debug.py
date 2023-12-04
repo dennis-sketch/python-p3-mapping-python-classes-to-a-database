bd806c17da9b3000b244b15a82f2b69746cdd951
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2023-12-04 20:11:05 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -6,3 +6,6 @@ from song import Song
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+Song.create_table()
+CURSOR.execute("PRAGMA table_info(songs)").fetchall()
+# => [(0, 'id', 'INTEGER', 0, None, 1), (1, 'name', 'TEXT', 0, None, 0), (2, 'album', 'TEXT', 0, None, 0)]
